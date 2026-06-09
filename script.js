3c8a11ba82513340acfe5a3989f664147665f7d7
Kwshal <kushal3738@gmail.com>
2026-06-09 19:21:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById('whiteboard');
+let ctx = canvas.getContext('2d');
+let drawing = false;
+
+canvas.addEventListener('mousedown', (e) => {
+     drawing = true;
+     ctx.beginPath();
+     ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener('mousemove', (e) => {
+     if (drawing) {
+          ctx.lineTo(e.offsetX, e.offsetY);
+          ctx.stroke();
+     }
+});
+
+canvas.addEventListener('mouseup', () => {
+     drawing = false;
+});
+
+canvas.addEventListener('mouseleave', () => {
+     drawing = false;
+});
+
