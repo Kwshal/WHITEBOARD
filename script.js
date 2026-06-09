a550a9b97e3f6e1b003dd6a5e368999841d443a4
kwshal <kushal3738@gmail.com>
2026-06-10 00:48:43 +0530

    feat: implement whiteboard page type switcher with interactive UI and styles

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById('whiteboard');
 let ctx = canvas.getContext('2d');
 let drawing = false;
+ctx.lineWidth = 10;
 
 canvas.addEventListener('mousedown', (e) => {
      drawing = true;
@@ -22,4 +23,18 @@ canvas.addEventListener('mouseup', () => {
 canvas.addEventListener('mouseleave', () => {
      drawing = false;
 });
+
+let icons = [...document.getElementsByClassName('page-type-icon')];
+
+icons.forEach(icon => {
+     icon.addEventListener('click', () => {
+          icons.forEach(i => i.classList.remove('selected'));
+          icon.classList.add('selected');
+     });
+});
+
+let button1 = document.getElementById('live');
+let button2 = document.getElementById('mobile');
+let button3 = document.getElementById('desktop');
+
 
