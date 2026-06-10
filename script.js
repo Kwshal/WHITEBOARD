c834032980755468fb686e55aad3494a93cd3553
Kwshal <kushal3738@gmail.com>
2026-06-10 11:46:18 +0530

    Add touch functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById('whiteboard');
 let ctx = canvas.getContext('2d');
 let drawing = false;
 
+
 canvas.addEventListener('mousedown', (e) => {
      drawing = true;
      ctx.beginPath();
@@ -23,3 +24,23 @@ canvas.addEventListener('mouseleave', () => {
      drawing = false;
 });
 
+canvas.addEventListener('touchstart', (e) => {
+     drawing = true;
+     ctx.beginPath();
+     ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener('touchmove', (e) => {
+     if (drawing) {
+          ctx.lineTo(e.offsetX, e.offsetY);
+          ctx.stroke();
+     }
+});
+
+canvas.addEventListener('touchend', () => {
+     drawing = false;
+});
+
+canvas.addEventListener('touchcancel', () => {
+     drawing = false;
+});
